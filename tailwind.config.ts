c4f929921291d035fb424fcdfabcd28388c93433
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-07-22 20:40:44 -0300

    add tailwind clasnames

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'puebla-green': '#BED359',
+        'puebla-blue': '#92C7D7',
+        'puebla-dark': '#005B49',
+      }
     },
   },
   plugins: [],
