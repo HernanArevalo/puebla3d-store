b253e7717de3653384d42038705bc8a89e5a5357
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-07-23 21:48:04 -0300

    add categories page

--- src/app/(shop)/category/[category]/page.tsx
+++ src/app/(shop)/category/[category]/page.tsx
@@ -2,44 +2,41 @@ export const revalidate = 60
 
 import { redirect } from "next/navigation";
 
-import { Gender } from "@prisma/client";
+import { Category } from "@prisma/client";
 
 import { getPaginatedProductsWithImages } from "@/actions";
 import { Pagination, ProductsGrid, Title } from "@/components";
 
 interface Props {
   params: {
-    gender: string
+    category: string
   },
   searchParams: {
     page?: string;
   }
 }
 
-export default async function GenderPage({ params, searchParams }: Props ) {
+export default async function categoryPage({ params, searchParams }: Props ) {
   
-  const { gender } = params 
-
+  const { category } = params 
 
   const page = searchParams.page ? parseInt( searchParams.page ) : 1;
 
   const {products, totalPages, currentPage} = await getPaginatedProductsWithImages({
     page,
-    gender: gender as Gender
+    category: category
   });
 
 
   if( products.length === 0){
-    redirect(`/gender/${gender}`)
+    redirect(`/category/${category}`)
   }
-
 
   return (
     <>
       <Title
-        title="Store"
-        subtitle={gender}
-        className="mb-2 capitalize"
+        title={category}
+        className="mt-14 mb-14 capitalize"
       />
 
       <ProductsGrid products={products}/>
