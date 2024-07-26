5c14dbd5776ffdc6e85fb1f38bdc7d0a099e7c8a
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-07-25 21:27:34 -0300

    change images and category data

--- src/app/(shop)/admin/products/page.tsx
+++ src/app/(shop)/admin/products/page.tsx
@@ -64,7 +64,7 @@ export default async function OrdersPage({ searchParams }: Props) {
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                   <Link href={`/product/${product.slug}`}>
                     <ProductImage 
-                      src={product.ProductImage[0]?.url} 
+                      src={product.images[0]} 
                       width={80} 
                       height={80} 
                       className="w-20 h-20 object-cover rounded"
@@ -81,7 +81,7 @@ export default async function OrdersPage({ searchParams }: Props) {
                   { currencyFormat(product.price)}
                 </td>
                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                  { product.category.name }
+                  { product.category }
                 </td>
                 <td className="text-sm text-gray-900 font-bold px-6 py-4 whitespace-nowrap">
                   { product.colors.join(',') }
