19ece1fb8a16002c1e3cda72b32a69b0ac51c64e
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-07-27 14:41:46 -0300

    separate codes page in components

--- src/app/(shop)/admin/codes/page.tsx
+++ src/app/(shop)/admin/codes/page.tsx
@@ -1,23 +1,11 @@
 export const revalidate = 0;
 
-// https://tailwindcomponents.com/component/hoverable-table
-import { getCodes, getPaginatedProductsWithImages } from '@/actions';
-import { Pagination, ProductImage, Title } from '@/components';
-import { currencyFormat } from '@/utils';
-
+import { Title } from '@/components';
 import Link from 'next/link';
-import { IoTicketOutline } from 'react-icons/io5';
-
-interface Props {
-  searchParams: {
-    page?: string;
-  };
-}
+import { CodesTable } from './components';
 
 
-export default async function OrdersPage({ searchParams }: Props) {
-
-  const codes = await getCodes();
+export default function OrdersPage() {
 
   return (
     <>
@@ -30,69 +18,7 @@ export default async function OrdersPage({ searchParams }: Props) {
       </div>
 
       <div className="mb-10">
-        <table className="min-w-full rounded overflow-hidden">
-          <thead className="bg-puebla-green border-b">
-            <tr>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                Código
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                Id
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
-                Descuento
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
-                Activo
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
-                Límite
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
-                Usos
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-
-            { codes.map(code => (
-              <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100" key={code.id}>
-
-                <td className="text-sm text-gray-900 font-bold px-6 py-4 whitespace-nowrap">
-                  { code.code }
-                </td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                  { code.id }
-                </td>
-                <td className="text-sm text-gray-900 font-bold px-6 py-4 whitespace-nowrap text-center">
-                  { code.discount }%
-                </td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap flex justify-center">
-                  { code.isActive?
-                    <IoTicketOutline color='green' size={20}/>
-                    :
-                    <IoTicketOutline color='red' size={20}/>
-                  }
-                </td>
-                <td className="text-sm text-gray-900 font-bold px-6 py-4 whitespace-nowrap text-center">
-                { code.usageLimit ? code.usageLimit : "-" }
-                </td>
-                <td className="text-sm text-gray-900 font-bold px-6 py-4 whitespace-nowrap text-center">
-                { code.usageCount }
-                </td>
-                
-
-              </tr>
-
-            ))
-
-            }
-
-
-
-          </tbody>
-        </table>
-
+        <CodesTable />
       </div>
     </>
   );
