f61d1f8f09a761480cd1456b6fe16bf14cde1a86
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-07-27 21:01:42 -0300

    finish form data handle

--- src/app/(shop)/admin/codes/components/CodeRow.tsx
+++ src/app/(shop)/admin/codes/components/CodeRow.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { createUpdateCode, deleteCode } from '@/actions';
 import { discountCode } from '@/interfaces'
+import clsx from 'clsx';
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { IoSave, IoTicketOutline, IoTrash } from 'react-icons/io5'
@@ -9,35 +11,95 @@ interface Props {
   discount: discountCode;
 }
 
-export const CodeRow = ({discount}: Props) => {
+export const CodeRow = ({ discount }: Props) => {
+  const [hasCodeChanged, setHasCodeChanged] = useState(false)
+  const [useExpiration, setUseExpiration] = useState(!!discount.expiresAt)
+  const [useLimitUses, setUseLimitUses] = useState(!!discount.usageLimit)
+
+
   const {
     handleSubmit,
     register,
     formState: { isValid },
     getValues,
     setValue,
+    watch
+
   } = useForm<discountCode>({
     defaultValues: {
       ...discount,
     },
   });
+  watch('isActive')
 
-  const onDeletingCode = () => {
 
+  const onSavingCode = async(data: discountCode) => {
+    const formData = new FormData();
+
+
+    if(useExpiration){
+      formData.append('expiresAt', data.expiresAt!.toString())
+    }
+    if(useLimitUses){
+      formData.append('usageLimit', data.usageLimit!.toString())
+    }
+    formData.append('id', data.id)
+    formData.append('code', data.code.toString())
+    formData.append('discount', data.discount.toString())
+    formData.append('updatedAt', new Date().toString())
+    formData.append('createdAt', data.createdAt.toString())
+    formData.append('isActive', data.isActive.toString())
+    formData.append('usageCount', data.usageCount.toString())
+
+    const { ok, code: updatedCode } = await createUpdateCode(formData)
+
+    setHasCodeChanged(false)
   }
-  const onSavingCode = () => {
 
+  const codeHasChanged = () => {
+    setHasCodeChanged(true)
   }
+
+  const changeActiveMode = () => {
+    const isActive = getValues('isActive')
+
+    if (isActive) {
+      setValue('isActive', false)
+    } else {
+      setValue('isActive', true)
+    }
+  }
+
+  const handleCorrectNumberValue = (event:any) => {
+    let value = getValues(event)
+
+
+    if (isNaN(value)) {
+      value = 0;
+    } else if (value <= 0) {
+      value = 1;
+    } else if (value > 100 && event == 'usageLimit') {
+      value = 100;
+    }
+
+    setValue('discount', value); // Actualiza el valor en react-hook-form
+  };
+
 
   return (
     <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100" key={discount.id}>
 
       <td className="pl-6 text-sm text-gray-900 font-bold py-4 whitespace-nowrap">
-        <input type="text" 
-               className=" focus:bg-puebla-blue focus:outline-none"
-                          {...register('code', { required: true })}
-
-               />
+        <input type="text"
+          className="focus:bg-puebla-blue focus:outline-none max-w-36 transition-all"
+          {...register('code', { required: true, onChange: ()=>setHasCodeChanged(true) })}
+          minLength={3}
+          maxLength={16}
+          style={{
+            outline: 'none',
+            boxShadow: 'none',
+          }}
+        />
       </td>
 
       <td className="text-sm text-gray-900 font-light py-4 whitespace-nowrap">
@@ -46,39 +108,62 @@ export const CodeRow = ({discount}: Props) => {
 
       <td className="text-sm text-gray-900 font-bold py-4 whitespace-nowrap text-center">
         <input type="number"
-               className=" focus:bg-puebla-blue focus:outline-none w-16"
-                          {...register('discount', { required: true })}
-
-               />
+          className=" focus:bg-puebla-blue focus:outline-none w-10 transition-all"
+          {...register('discount', { required: true, max: 100, min: 1, maxLength: 3, onChange: ()=>{handleCorrectNumberValue('discount');setHasCodeChanged(true)} })}
+          min={0}
+          max={100}
+          maxLength={3}
+          style={{
+            outline: 'none',
+            boxShadow: 'none',
+          }}
+        />
         <span>%</span>
       </td>
 
       <td className="text-sm text-gray-900 font-bold py-4 whitespace-nowrap text-center">
-        <input type="datetime-local" 
-                    {...register('expiresAt', { required: true })}
-
-        />
+        <div className="flex justify-center gap-3">
+          <input type="checkbox" 
+                 className="accent-puebla-dark transition-all cursor-pointer disabled:text-gray-500" 
+                 checked={useExpiration} 
+                 onChange={()=>{setUseExpiration(!useExpiration)}}
+                 />
+          <input type="datetime-local"
+            {...register('expiresAt', { onChange: ()=>setHasCodeChanged(true) })}
+            className="disabled:text-gray-500" 
+            disabled={!useExpiration}
+            min={new Date().toISOString().slice(0,16)}
+          />
+        </div>
       </td>
 
-      <td className="text-sm text-gray-900 font-light py-4 whitespace-nowrap mx-auto text-center">
-        <span className="flex justify-center">
-        {discount.isActive ?
-          <IoTicketOutline color='green' size={20} />
-          :
-          <IoTicketOutline color='red' size={20} />
-        }
-
-        </span>
+      <td className="text-sm text-gray-900 font-light py-4 whitespace-nowrap mx-auto text-center flex justify-center">
+        <button className={
+          clsx(
+            "flex justify-center items-center hover:bg-gray-600 p-2 rounded-full transition-all",
+            { 'bg-green-600': getValues('isActive') },
+            { 'bg-red-600': !getValues('isActive') }
+          )}
+          onClick={()=>{changeActiveMode(); setHasCodeChanged(true)}}>
+          <IoTicketOutline size={20} />
+        </button>
       </td>
 
       <td className="text-sm text-gray-900 font-bold py-4 whitespace-nowrap text-center">
-        <div className="flex flex-row justify-center align-center gap-3 pl-10">
-          <input type="checkbox" className="mr-2"/>
-          <input type="number" className="w-16 focus:bg-puebla-blue focus:outline-none"
-                      {...register('usageLimit', { required: true })}
+        <div className="flex justify-center gap-3">
+          <input type="checkbox" className="accent-puebla-dark transition-all cursor-pointer" checked={useLimitUses} onChange={()=>{setUseLimitUses(!useLimitUses)}}/>
 
+          <input type="number" 
+                 className="w-16 focus:bg-puebla-blue focus:outline-none transition-all disabled:text-gray-500"
+                 style={{
+                  outline: 'none',
+                  boxShadow: 'none',
+                }}
+                {...register('usageLimit', { min: 0, onChange: codeHasChanged })}
+                min={0}
+                disabled={!useLimitUses}
+                onChange={()=> handleCorrectNumberValue('usageLimit')}
           />
-          
         </div>
       </td>
 
@@ -87,10 +172,12 @@ export const CodeRow = ({discount}: Props) => {
       </td>
 
       <td className="text-center flex md:flex-row flex-col gap-2 py-4">
-        <button className="p-2 bg-red-800 rounded" onClick={onDeletingCode}>
+        <button className="p-2 bg-red-700 rounded" onClick={()=>{deleteCode(discount)}}>
           <IoTrash color='white' />
         </button>
-        <button className="p-2 bg-blue-800 rounded" onClick={onSavingCode}>
+        <button disabled={!hasCodeChanged} 
+                className={`p-2 rounded transition-all ${hasCodeChanged ? "bg-blue-600 hover:bg-blue-800" : "bg-gray-400 cursor-default"}`} 
+                onClick={handleSubmit(onSavingCode)}>
           <IoSave color='white' />
         </button>
       </td>
