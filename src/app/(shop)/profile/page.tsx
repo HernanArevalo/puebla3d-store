99233cbed6fb603f426a74163f5de8744be6f57c
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-07-30 18:03:54 -0300

    restyle profile page

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from '@/auth.config';
-import { Title } from '@/components';
+import { CodesTable, Title } from '@/components';
 import { titleFont } from '@/config/fonts';
 import { User } from '@/interfaces';
 import Image from 'next/image';
@@ -7,29 +7,30 @@ import { redirect } from 'next/navigation';
 
 export default async function ProfilePage() {
   const session = await auth();
-
-
   if (!session?.user) {
-    // redirect('/auth/login?returnTo=/profile');
     redirect("/");
   }
   const user = session.user as User
 
   return (
-    <div>
-      <Title title="Profile" />
-      <pre>{JSON.stringify(user, null, 2)}</pre>
+    <div className="flex flex-col gap-4 h-full">
       <Title title="Perfil" />
-      <div className="flex flex-row gap-10">
+      <div className="flex flex-row gap-10 items-center">
         { user.image &&
-          <Image className="rounded-full border-puebla-dark border-4" src={session.user.image} width={100} height={100} alt='profile picture'/>
+          <Image className="rounded-full border-puebla-dark border-4" src={user.image} width={100} height={100} alt='profile picture' />
         }
         <div className="">
-          <h2 className={`${titleFont.className} font-semibold text-xl`}>{session.user.name}</h2>
-          <span>{user.role}</span>
+          <div className="flex flex-row items-center gap-3">
+            <h2 className={`${titleFont.className} font-semibold text-xl`}>{session.user.name}</h2>
+            <span className="bg-gray-500 bg-opacity-60 p-1 text-xs">{user.role}</span>
+          </div>
           <span>{user.email}</span>
         </div>
-        
+      </div>
+
+      <div>
+        <Title title="Órdenes" />
+        <CodesTable />
       </div>
     </div>
   );
