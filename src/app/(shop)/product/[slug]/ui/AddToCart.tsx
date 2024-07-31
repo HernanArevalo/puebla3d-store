68000e2a8970bedf02649712a6e62838f3b9f45e
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-07-30 21:02:56 -0300

    reorganize data and styles

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { QuantitySelector, SizeSelector } from '@/components';
-import { CartProduct, Product, Size } from '@/interfaces';
+import { ColorSelector, QuantitySelector, SizeSelector } from '@/components';
+import { titleFont } from '@/config/fonts';
+import { CartProduct, Product, ProductColor, ProductInStock } from '@/interfaces';
 import { useCartStore } from '@/store';
 import { useState } from 'react';
 
@@ -13,42 +14,70 @@ export const AddToCart = ({ product }: Props) => {
 
   const addProductToCart = useCartStore(state => state.addProductToCart)
 
-  const [size, setSize] = useState<Size | undefined>();
+  const [selectedSize, setSelectedSize] = useState<ProductInStock>(product.inStock[0])
+  const [selectedColor, setSelectedColor] = useState<ProductColor>(selectedSize.colors[0])
+
+  const [availableColors, setAvailableColors] = useState(selectedSize.colors)
+
   const [quantity, setQuantity] = useState<number>(1);
+
+  const sizes = Array.from(new Set(product.inStock.map(item => item.size)))
   const [posted, setPosted] = useState(false);
 
   const AddToCart = () => {
     setPosted(true);
-    if (!size) return;
+    if (!selectedSize) return;
 
     const cartProduct: CartProduct = {
       id: product.id,
       image: product.images[0],
-      price: product.price,
+      price: selectedSize.price,
       quantity: quantity,
-      size: size,
+      size: selectedSize.size,
       slug: product.slug,
       title: product.title,
     }
     addProductToCart(cartProduct)
 
-    setSize(undefined);
+    setSelectedSize(product.inStock[0])
     setQuantity(1);
     setPosted(false);
   };
 
+  const onChangeSize = (size: string) => {
+    const newSize = product.inStock.find(prod => prod.size === size)
+    if (newSize) {
+      setSelectedSize(newSize)
+      setSelectedColor(selectedSize.colors[0])
+    }
+  }
+
   return (
     <>
+      <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+        {product.title}
+      </h1>
+      <p className="text-lg mb-5">${selectedSize.price}</p>
+
       {/* Size picker */}
       <SizeSelector
-        availableSizes={product.sizes}
-        selectedSize={size}
-        onSizeChanged={setSize}
+        availableSizes={sizes}
+        selectedSize={selectedSize.size}
+        onSizeChanged={onChangeSize}
       />
-      {posted && !size && 
-        <p className='text-red-500'>Select a size</p>
+      {posted  &&
+        <p className='text-red-500'>Seleccioná un tamaño</p>
       }
 
+      <ColorSelector
+        availableColors={selectedSize.colors}
+        selectedColor={selectedColor}
+        onColorChanged={(col:ProductColor)=> setSelectedColor(col)}
+      />
+
+      {posted &&
+        <p className='text-red-500'>Seleccioná un tamaño</p>
+      }
       {/* Quantity picker */}
       <QuantitySelector quantity={quantity} onQuantityChanged={setQuantity} />
 
