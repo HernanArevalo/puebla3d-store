ffab6c82731cf3f8c6383de4652d40996e67fd2a
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-06 18:24:24 -0300

    add SubTitle

--- src/components/ui/title/Title.tsx
+++ src/components/ui/title/Title.tsx
@@ -23,3 +23,18 @@ export const Title = ({title, subtitle, className}: Props) => {
     </div>
   )
 }
+
+export const SubTitle = ({ title, subtitle, className }: Props) => {
+  return (
+    <div className={`mt-3 ${className}`}>
+      <h1 className={`${titleFont.className} antialiased text-xl font-semibold mb-4`}>
+        {title}
+      </h1>
+      {subtitle && (
+        <h3 className="text-xl mb-10">
+          {subtitle}
+        </h3>
+      )}
+    </div>
+  );
+};
