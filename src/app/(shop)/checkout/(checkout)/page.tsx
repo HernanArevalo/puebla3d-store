04cee9fc0fdbb47fcd85f2e1d60ceb8c39b04b42
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-07 23:42:36 -0300

    styles

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,9 +1,6 @@
 import { Title } from "@/components";
-import Image from "next/image";
 import Link from "next/link";
 import { PlaceOrder, ProductsInCart } from "./ui";
-
-
 
 
 export default function CheckoutPage() {
@@ -11,14 +8,13 @@ export default function CheckoutPage() {
     <div className="flex justify-center items-start mb-72 px-10 sm:px-10">
       
       <div className="flex flex-col w-[1000px]">
-        <Title title="Verify order"/>
+        <Title title="Verificar Orden"/>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
           {/* Cart */}
           <div className="flex flex-col mt-5">
-            <span className="text-xl">Adjust elements</span>
             <Link href='/cart' className="mb-5 underline">
-            Edit cart
+            Editar carro
             </Link>
 
           <ProductsInCart />
