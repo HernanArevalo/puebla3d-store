97b88c23e8f3ebe625b0e08fb9f86ca4a35e321c
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-07 23:41:57 -0300

    add signIn button

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,12 +1,11 @@
 import Link from 'next/link';
-import { Title } from '@/components';
+import { SignInButton, Title } from '@/components';
 import { OrderSummary, ProductsInCart } from './ui';
+import { auth } from '@/auth.config';
 
-export default function CartPage() {
+export default async function CartPage() {
+  const session = await auth();
 
-
-
-  
   return (
     <div className='flex justify-center items-start mb-72 px-10 sm:px-10'>
       <div className='flex flex-col w-[1000px]'>
@@ -24,17 +23,25 @@ export default function CartPage() {
 
           {/* Checkout */}
           <div className='bg-white rounded-xl shadow-xl p-7 h-fit'>
-            <h2 className='text-2xl mb-2 font-bold'>Resumen de orden</h2>
+            <h2 className='text-2xl mb-2 font-bold'>Resumen de orden:</h2>
 
             <OrderSummary />
 
-            <div className='mt-5 mb-2 w-full'>
-              <Link
-                href='/checkout/adress'
-                className='flex btn-primary justify-center'
-              >
-                Continuar compra
-              </Link>
+
+            <div className='mt-5 mb-2 w-full flex flex-col justify-center'>
+              {!session ?
+                <div className="flex flex-col w-full justify-center items-center gap-3">
+                  <h2 className='text-2xl mb-2 font-bold mt-5 w-full text-left'>Continuar compra:</h2>
+                  <SignInButton />
+                </div>
+                :
+                <Link
+                  href='/checkout/adress'
+                  className='flex btn-primary justify-center w-full'
+                >
+                  Continuar compra
+                </Link>
+              }
             </div>
           </div>
         </div>
