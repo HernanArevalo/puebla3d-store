4b30b5d37a6c7119c83d0efc27b94dedfd3525a3
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-13 19:03:42 -0300

    add SubtitleNoMargin component

--- src/components/ui/title/Title.tsx
+++ src/components/ui/title/Title.tsx
@@ -24,6 +24,24 @@ export const Title = ({ title, subtitle, className }: Props) => {
   )
 }
 
+export const SubtitleNoMargin = ({ title, subtitle, className }: Props) => {
+  return (
+    <div
+      className={`${className}`}
+    >
+      <h1 className={`${titleFont.className} antialiased text-2xl font-normal`}>
+        {title}
+      </h1>
+
+      {subtitle && (
+        <h3 className="text-xl mb-10">
+          {subtitle}
+        </h3>
+      )}
+    </div>
+  )
+}
+
 export const SubTitle = ({ title, subtitle, className }: Props) => {
   return (
     <div className={`mt-3 ${className}`}>
