50470188c64c9f9bfdb2a6bc5ab10fd9ee4a04dc
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-13 19:30:29 -0300

    update data showed

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 import Image from 'next/image';
 
 import { getOrderById } from '@/actions';
-import { OrderStatus, PayPalButton, Title } from '@/components';
+import { OrderStatus, PayPalButton, ProductImage, SubtitleNM, TitleNM } from '@/components';
 import { currencyFormat } from '@/utils';
 
 interface Props {
@@ -23,13 +23,15 @@ export default async function OrderPage({ params }: Props) {
   const address = order?.OrderAddress;
 
   return (
-    <div className="flex justify-center items-start mb-72 px-10 sm:px-10">
+    <div className="flex justify-center items-start mb-24 px-10 sm:px-10">
       <div className="flex flex-col w-[1000px]">
-        <Title title={`Order #${id.split('-').at(-1)}`} />
+        <TitleNM title={`Pedido confirmado`} />
+        <SubtitleNM title={`#${id.split('-').at(-1)}`} className='font-thin mb-5' />
+
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
           {/* Cart */}
-          <div className="flex flex-col mt-5">
+          <div className="flex flex-col">
             <OrderStatus isPaid={order?.isPaid ?? false} />
 
             {order?.OrderItems.map((item) => (
@@ -37,25 +39,26 @@ export default async function OrderPage({ params }: Props) {
                 className="flex gap-5 mb-2"
                 key={item.product.slug + '-' + item.size}
               >
-                <Image
-                  src={`/products/${item.product.ProductImage[0].url}`}
+                <ProductImage
+                  src={item.product.ProductImage[0].url}
                   width={100}
                   height={100}
+                  style={{ width: '100px', height: '100px' }}
                   alt={item.product.title}
-                  className="rounded"
+                  className='rounded'
                 />
                 <div>
                   <p>
                     {item.product.title} - {item.size}
                   </p>
                   <p>
-                    {item.price} x {item.quantity}
+                    {currencyFormat(item.price)} x {item.quantity}
                   </p>
                   <p className="font-bold">
                     Subtotal: {currencyFormat(item.price * item.quantity)}
                   </p>
 
-                  <button className="underline mt-1">Delete</button>
+                  <button className="underline mt-1">Borrar</button>
                 </div>
               </div>
             ))}
@@ -63,43 +66,36 @@ export default async function OrderPage({ params }: Props) {
 
           {/* Checkout */}
           <div className="bg-white rounded-xl shadow-xl p-7 h-fit">
-            <h2 className="text-2xl mb-2">Adress Delivry</h2>
+            <h2 className="text-2xl mb-2 font-semibold">Datos:</h2>
             <div className="mb-10">
-              <p className="text-xl">
-                {address?.firstName} {address?.lastName}
-              </p>
+              <p className="text-xl">{address?.firstName} {address?.lastName}</p>
+              <p>{address?.phone}</p>
               <p>{address?.address}</p>
               <p>{address?.address2}</p>
-              <p>{address?.postalCode}</p>
-              <p>
-                {address?.city}, {address?.countryId}
+              <p>{address?.city} ({address?.postalCode}) - Argentina</p>
+              <p className="flex flex-row items-center gap-2">Método de entrega:
+                <span className="capitalize font-semibold">{order?.shippingMethod}</span>
               </p>
-              <p>{address?.phone}</p>
             </div>
 
             {/* Divider */}
             <div className="w-full h-0.5 rounded bg-gray-300 mb-10" />
 
-            <h2 className="text-2xl mb-2">Order resume</h2>
+            <h2 className="text-2xl mb-2 font-semibold">Resumen de la compra:</h2>
 
             <div className="grid grid-cols-2">
-              <span className="font-semibold">Products</span>
-              <span className="text-right">
-                {order?.items !== 1 ? `${order?.items} artcicles` : `1 article`}
-              </span>
+              <span className="font-semibold">Productos</span>
+              <span className='text-right'>{order!.items !== 1 ? `${order!.items} artículos` : `1 artículo`}</span>
 
-              <span className="font-semibold">Subtotal</span>
-              <span className="text-right">
-                {currencyFormat(order!.subTotal)}
-              </span>
+              <span className='text-3xl font-semibold'>Total:</span>
+              <span className='text-3xl text-right'>{currencyFormat(order!.subTotal)}</span>
 
-              <span className="font-semibold">Taxes (15%)</span>
-              <span className="text-right">{currencyFormat(order!.tax)}</span>
 
-              <span className="mt-5 text-2xl font-semibold">Total:</span>
-              <span className="mt-5 text-2xl text-right">
-                {currencyFormat(order!.total)}
-              </span>
+              <span className='mt-8 text-xl font-semibold'>Con transferencia:</span>
+              <span className='mt-8 text-xl text-right'>{currencyFormat(order!.total)}</span>
+
+              <span className="font-semibold">Descuento (10%):</span>
+              <span className='text-right'>{currencyFormat(order!.tax)}</span>
             </div>
 
             <div className="mt-5 mb-2 w-full">
