b0a88583fa6f92a25c5e56b391ebce3c583213db
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-21 19:58:40 -0300

    create order interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,6 +2,7 @@ export * from './address.interface';
 export * from './code.interface';
 export * from './country.interface';
 export * from './category.interface';
+export * from './order.interface';
 export * from './paypal.interface';
 export * from './product.interface';
 export * from './province.interface';
---
+++ src/interfaces/order.interface.ts
@@ -0,0 +1,53 @@
+
+export interface OrderItem {
+  title:    string;
+  slug:    string;
+  quantity: number;
+  price:    number;
+  size:     string;
+  color:    string;
+  image:    string;
+}
+
+export interface OrderAddress {
+  id:         String;
+  firstName:  String;
+  lastName:   String;
+  address:    String;
+  address2?:   String | null;
+  postalCode: String;
+  city:       String;
+  phone:      String;
+
+  //* relations
+  countryId:  String;
+  provinceId: String;
+
+  orderId: String;
+}
+
+export interface Order{
+    id: string
+    total: number
+    items: number
+    isPaid:boolean
+    status: string
+  
+    shippingMethod?: string | null
+    shippingAmount: number
+  
+    paidAt?: Date | null
+    paymentMethod: string | null
+    transactionImage?: string | null
+    transactionId?: string | null
+  
+    createdAt: Date
+    updatedAt: Date
+  
+    userId: string
+
+    OrderItems:   OrderItem[];
+    OrderAddress?: OrderAddress | null;
+  
+    discountCodeId?: string | null;
+}
