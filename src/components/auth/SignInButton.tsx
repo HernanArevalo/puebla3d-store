325a1bf16afc35cd9bd22025fee8283e78d48db7
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-22 23:54:49 -0300

    change image url by react-icons component

--- src/components/auth/SignInButton.tsx
+++ src/components/auth/SignInButton.tsx
@@ -1,18 +1,13 @@
 "use client"
 import { signIn } from 'next-auth/react'
 import Image from 'next/image'
+import { FcGoogle } from 'react-icons/fc'
 
 export const SignInButton = () => {
   return (
-    <button className="w-fit p-3 flex items-center gap-1 bg-gray-100 border-puebla-blue border-4 rounded transition-all" onClick={() => signIn('google')}>
-      <Image
-        loading="lazy"
-        height="24"
-        width="24"
-        src="https://authjs.dev/img/providers/google.svg"
-        alt="Google Logo"
-      />
-      <span className="text-xl ml-2">Iniciar sesión</span>
+    <button className="flex flex-row justify-center items-center w-fit p-3 gap-1 bg-gray-100 border-puebla-blue border-2 rounded transition-all hover:border-black" onClick={() => signIn('google')}>
+      <FcGoogle size={ 28 }/>
+      <span className="text-xl ml-2 font-semibold">Iniciar sesión</span>
     </button>
   )
 }
