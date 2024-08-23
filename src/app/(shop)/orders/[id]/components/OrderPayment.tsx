fa3f44d26e1df4448412ac5f8f458830bdcb2f15
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-23 00:08:55 -0300

    add transfer payment data

--- src/app/(shop)/orders/[id]/components/OrderPayment.tsx
+++ src/app/(shop)/orders/[id]/components/OrderPayment.tsx
@@ -3,56 +3,64 @@
 import { SubtitleNM } from "@/components";
 import { currencyFormat, discount } from "@/utils";
 import { useState } from "react";
+import { BiSolidCopy } from "react-icons/bi";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import { IoCopy } from "react-icons/io5";
+import { RiFileCopyLine } from "react-icons/ri";
 
 interface Params {
   productsPrice: number;
-   shippingPrice: number;
+  shippingPrice: number;
+
+  paymentMethodSelected: string | undefined;
+  setPaymentMethodSelected: (method: string | undefined) => void
 }
 
-export const OrderPayment = ({productsPrice, shippingPrice}: Params ) => {
-  const [paymentMethodSelected, setPaymentMethodSelected] = useState<string|undefined>()
-
+export const OrderPayment = ({ productsPrice, shippingPrice, paymentMethodSelected, setPaymentMethodSelected }: Params) => {
 
   const priceMercadopago = productsPrice + shippingPrice
   const priceTransfer = (productsPrice + shippingPrice) * discount
 
   return (
     <>
-      {paymentMethodSelected == undefined ?
-        <div className="w-full flex flex-row justify-center gap-14">
-          <button className="btn-dark" onClick={()=>setPaymentMethodSelected('MP')}>Mercado Pago</button>
-          <button className="btn-dark" onClick={()=>setPaymentMethodSelected('T')}>Transferencia</button>
-        </div>
-        :
-        <div className="flex flex-col bg-puebla-blue rounded-md p-3 gap-5">
-        <button onClick={()=>setPaymentMethodSelected(undefined)} className="bg-gray-300 text-black rounded-md w-fit p-2">
+      <div className="flex flex-col bg-puebla-blue bg-opacity-50 rounded-md p-3 gap-5">
+        <button onClick={() => setPaymentMethodSelected(undefined)} className="bg-gray-300 text-black rounded-md w-fit p-2">
           <IoMdArrowRoundBack />
         </button>
-        { paymentMethodSelected == 'T' ?
+        {paymentMethodSelected == 'T' ?
           <div>
-              <h2 className="text-xl mb-2 font-semibold">Pago con trasferencia:</h2>
-              <div className="flex flex-col gap-2">
-                <div className="bg-white w-fit p-1 rounded-md">
-                  A pagar:
-                  <span className="font-semibold"> { currencyFormat(priceTransfer) }</span>
+            <h2 className="text-xl mb-2 font-semibold">Pago con trasferencia:</h2>
+            <div className="flex flex-col gap-2">
+              <div className="w-full flex justify-center">
+                <div className="bg-white w-fit p-3 rounded-md font-semibold text-xl mb-4">
+                  {currencyFormat(priceTransfer)}
                 </div>
-                <div className="bg-white w-fit p-1 rounded-md">
-                  <h4 className="font-bold">Datos de la cuenta:</h4>
-                  <div className="grid grid-cols-2">
-                    <span>CVU:</span>
-                    <span>nan.arevalo</span>
-
-                    <span>ALIAS:</span>
-                    <span>nan.arevalo</span>
-
-                    <span>NOMBRE:</span>
-                    <span>HERNÁN ARÉVALO</span>
-
-                  </div>
-                </div>
-
               </div>
+              <div className="bg-white w-fit p-3 rounded-md">
+                <h4 className="font-bold  text-xl mb-3">Datos de la cuenta:</h4>
+                <div className="grid grid-cols-2">
+                  <span className="font-semibold">CVU:</span>
+                  <div className="flex flex-row gap-2">
+                    <span>0000168300000003939550</span>
+                    <BiSolidCopy />
+                  </div>
+
+                  <span className="font-semibold">ALIAS:</span>
+                  <div className="flex flex-row gap-2">
+                    <span>nan.arevalo</span>
+                    <BiSolidCopy />
+                  </div>
+
+                  <span className="font-semibold">NOMBRE:</span>
+                  <div className="flex flex-row gap-2">
+                    <span>HERNÁN ARÉVALO</span>
+                    <BiSolidCopy />
+                  </div>
+
+                </div>
+              </div>
+
+            </div>
 
           </div>
           :
@@ -60,8 +68,8 @@ export const OrderPayment = ({productsPrice, shippingPrice}: Params ) => {
             <h2 className="text-xl mb-2 font-semibold">Pago con MercadoPago:</h2>
           </div>
         }
-        </div>
-      }
+      </div>
+
     </>
   )
 }
