96b50e05bdcbcee5c6f3f994c6bb57f3442e4283
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-09-13 19:56:52 -0300

    finish action process

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -2,34 +2,64 @@
 
 import { revalidatePath } from 'next/cache';
 import { z } from 'zod';
-import { Gender, Product, Size } from '@prisma/client';
+import { Product } from '@prisma/client';
 import prisma from '@/lib/prisma';
 
-import {v2 as cloudinary} from 'cloudinary';
-cloudinary.config( process.env.CLOUDINARY_URL ?? '');
+import { v2 as cloudinary } from 'cloudinary';
+cloudinary.config(process.env.CLOUDINARY_URL ?? '');
 
 const productSchema = z.object({
   id: z.string().uuid().optional().nullable(),
   title: z.string().min(3).max(255),
   slug: z.string().min(3).max(255),
   description: z.string(),
-  price: z.coerce
-    .number()
-    .min(0)
-    .transform((val) => Number(val.toFixed(2))),
-  inStock: z.coerce
-    .number()
-    .min(0)
-    .transform((val) => Number(val.toFixed(0))),
-  categoryId: z.string().uuid(),
-  sizes: z.coerce.string().transform((val) => val.split(',')),
-  tags: z.string(),
-  gender: z.nativeEnum(Gender),
+  enabled: z.boolean().optional().nullable(), // Cambiado a booleano
+  unique: z.boolean().optional().nullable(), // Cambiado a booleano
+  useStock: z.boolean().optional(), // Cambiado a booleano
+  inStock: z.array(
+    z.object({
+      id: z.string().uuid().optional().nullable(),
+      size: z.string(),
+      price: z.coerce
+        .number()
+        .min(0)
+        .transform((val) => Number(val.toFixed(2))),
+      oldPrice: z.coerce
+        .number()
+        .min(0)
+        .transform((val) => Number(val.toFixed(2)))
+        .nullable(),
+      colors: z.array(
+        z.object({
+          id: z.string().uuid().optional().nullable(),
+          name: z.string(),
+          stock: z.coerce
+            .number()
+            .min(0)
+            .transform((val) => Number(val.toFixed(0))),
+        })
+      ),
+    })
+  ),
+  tags: z.string(), // Cambiado a array de cadenas
+  // category: z.string().uuid(),
+  category: z.string(),
 });
 
+const transformData = (formData: FormData) => {
+  const data = Object.fromEntries(formData.entries()); // Cambiado para asegurar el tipo
+
+  return {
+    ...data,
+    enabled: data.enabled === 'true', // Convertir a booleano
+    useStock: data.useStock === 'true', // Convertir a booleano
+    inStock: data.inStock ? JSON.parse(data.inStock as string) : [], // Parsear JSON a array
+  };
+};
+
 export const createUpdateProduct = async (formData: FormData) => {
-  const data = Object.fromEntries(formData);
-  const productParsed = productSchema.safeParse(data);
+  const transformedData = transformData(formData);
+  const productParsed = productSchema.safeParse(transformedData);
 
   if (!productParsed.success) {
     console.log(productParsed.error);
@@ -44,53 +74,107 @@ export const createUpdateProduct = async (formData: FormData) => {
   try {
     const prismaTx = await prisma.$transaction(async (ts) => {
       let product: Product;
+
       const tagsArray = rest.tags
         .split(',')
         .map((tag) => tag.trim().toLowerCase());
+
+      // Convierte el array `inStock` en la forma esperada por Prisma
+      const inStockData = rest.inStock.map((item) => ({
+        id: item.id,
+        size: item.size,
+        price: item.price,
+        oldPrice: item.oldPrice,
+        colors: item.colors.map((color) => ({
+          name: color.name,
+          stock: color.stock,
+          id: color.id ? color.id : undefined,
+        })),
+      }));
 
       if (id) {
         // update
         product = await prisma.product.update({
           where: { id },
           data: {
-            ...rest,
-            sizes: {
-              set: rest.sizes as Size[],
-            },
+            title: rest.title,
+            description: rest.description,
+            slug: rest.slug,
             tags: {
               set: tagsArray,
             },
+            enabled: rest.enabled ? rest.enabled : undefined,
+            useStock: rest.useStock,
+            category: rest.category,
           },
         });
       } else {
+        // create
         product = await prisma.product.create({
           data: {
-            ...rest,
-            sizes: {
-              set: rest.sizes as Size[],
-            },
+            title: rest.title,
+            description: rest.description,
+            slug: rest.slug,
             tags: {
               set: tagsArray,
             },
+            enabled: rest.enabled ? rest.enabled : undefined,
+            useStock: rest.useStock,
+            category: rest.category,
           },
         });
       }
 
+      // load inStock process
+      for (const stock of inStockData) {
+        const { colors, ...restStock } = stock;
+        console.log(stock);
+
+        if (restStock.id && product.id) {
+          const dbInStock = await ts.inStock.update({
+            where: {
+              id: restStock.id,
+            },
+            data: {
+              price: restStock.price,
+              size: restStock.size,
+            },
+          });
+          for (const color of colors) {
+            console.log(color);
+            if (color.id) {
+              const dbColors = await ts.color.update({
+                where: {
+                  id: color.id,
+                },
+                data: { ...color },
+              });
+            } else {
+              const dbColor = await prisma.color.createMany({
+                data: {
+                  ...color,
+                  inStockId: dbInStock.id,
+                },
+              });
+            }
+          }
+        }
+      }
+
       // load images process
-      if ( formData.getAll('images')) {
+      if (formData.getAll('images')) {
         const images = await uploadImages(formData.getAll('images') as File[]);
 
         if (!images) {
-          throw new Error('error updating images, rollingback')
+          throw new Error('error updating images, rollingback');
         }
 
         await prisma.productImage.createMany({
-          data: images.map(image => ({
+          data: images.map((image) => ({
             url: image!,
-            productId: product.id
-          }))
-        })
-
+            productId: product.id,
+          })),
+        });
       }
 
       return {
@@ -105,71 +189,63 @@ export const createUpdateProduct = async (formData: FormData) => {
 
     return {
       ok: true,
-      product: prismaTx.product
+      product: prismaTx.product,
     };
-  } catch (e){
-    return{
-      ok:false,
-      message: 'Error updating/creating'
-    }
+  } catch (e) {
+    console.log(e);
+    return {
+      ok: false,
+      message: 'Error updating/creating',
+    };
   }
 };
 
-
-const uploadImages = async( images: File[]) => {
-
+const uploadImages = async (images: File[]) => {
   try {
-    const uploadPromises = images.map( async(image) => {
-
+    const uploadPromises = images.map(async (image) => {
       try {
         const buffer = await image.arrayBuffer();
         const base64image = Buffer.from(buffer).toString('base64');
-  
-        return cloudinary.uploader.upload(`data:image/png;base64,${ base64image }`)
-          .then( r => r.secure_url)
-  
-          
-        } catch (error) {
-          console.log(error);
-          return null
-        }
-    })
 
-    const uploadedImages = await Promise.all( uploadPromises )
-    return uploadedImages
-        
+        return cloudinary.uploader
+          .upload(`data:image/png;base64,${base64image}`)
+          .then((r) => r.secure_url);
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+    });
 
+    const uploadedImages = await Promise.all(uploadPromises);
+    return uploadedImages;
   } catch (error) {
     console.log(error);
-    return null
+    return null;
   }
-}
+};
 
-const loadVoucherImage = async( images: File[]) => {
-
+const loadVoucherImage = async (images: File[]) => {
   try {
-    const uploadPromises = images.map( async(image) => {
-
+    const uploadPromises = images.map(async (image) => {
       try {
         const buffer = await image.arrayBuffer();
         const base64image = Buffer.from(buffer).toString('base64');
-  
-        return cloudinary.uploader.upload(`data:image/png;base64,${ base64image }`, {folder: "vouchers"})
-          .then( r => r.secure_url)
-  
-          
-        } catch (error) {
-          console.log(error);
-          return null
-        }
-    })
 
-    const uploadedImages = await Promise.all( uploadPromises )
-    return uploadedImages
-        
+        return cloudinary.uploader
+          .upload(`data:image/png;base64,${base64image}`, {
+            folder: 'vouchers',
+          })
+          .then((r) => r.secure_url);
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+    });
 
+    const uploadedImages = await Promise.all(uploadPromises);
+    return uploadedImages;
   } catch (error) {
     console.log(error);
-    return null
+    return null;
   }
-}
+};
