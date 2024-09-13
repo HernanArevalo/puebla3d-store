40e03d70a6e5abc04e5d3c991dedbbb13720912b
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-09-13 13:58:27 -0300

    add react-dropzone library

--- src/app/(shop)/orders/[id]/components/OrderPayment.tsx
+++ src/app/(shop)/orders/[id]/components/OrderPayment.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { SubtitleNM } from "@/components";
-import { currencyFormat, discount } from "@/utils";
-import { useState } from "react";
+import { SweetAlert } from "@/components";
+import { aliasBanco, cbuBanco, currencyFormat, discount, titularBanco } from "@/utils";
 import { BiSolidCopy } from "react-icons/bi";
 import { IoMdArrowRoundBack } from "react-icons/io";
-import { IoCopy } from "react-icons/io5";
-import { RiFileCopyLine } from "react-icons/ri";
+import { useDropzone } from 'react-dropzone';
+import { RiDeleteBin6Fill } from "react-icons/ri";
+import { useState } from "react";
 
 interface Params {
   productsPrice: number;
@@ -17,9 +17,29 @@ interface Params {
 }
 
 export const OrderPayment = ({ productsPrice, shippingPrice, paymentMethodSelected, setPaymentMethodSelected }: Params) => {
+  const [acceptedFiles, setAcceptedFiles] = useState<File[]>([]);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      'application/pdf': ['.pdf'],
+      'image/jpeg': ['.jpg', '.jpeg'],
+      'image/png': ['.png']
+    },
+    multiple: false,
+    onDrop: (files) => {
+      setAcceptedFiles(files); // Guarda los archivos aceptados en el estado
+    }
+  });
 
   const priceMercadopago = productsPrice + shippingPrice
-  const priceTransfer = (productsPrice + shippingPrice) * discount
+  const priceTransfer = (productsPrice + shippingPrice) * (1 - discount)
+
+  const copyToClipboard = (toCopy: string) => {
+    navigator.clipboard.writeText(toCopy).then(() => {
+      SweetAlert({ title: 'Copiado en el portapapeles!' })
+    }).catch((err) => {
+      console.error('Error al copiar el texto: ', err);
+    });
+  };
 
   return (
     <>
@@ -31,35 +51,60 @@ export const OrderPayment = ({ productsPrice, shippingPrice, paymentMethodSelect
           <div>
             <h2 className="text-xl mb-2 font-semibold">Pago con trasferencia:</h2>
             <div className="flex flex-col gap-2">
-              <div className="w-full flex justify-center">
-                <div className="bg-white w-fit p-3 rounded-md font-semibold text-xl mb-4">
-                  {currencyFormat(priceTransfer)}
+              <div className="bg-white p-3 rounded-md w-full">
+                <div className="grid grid-cols-2">
+                  <span className='mt-4 text-xl font-semibold'>Total:</span>
+                  <span className='mt-4 text-xl text-right font-semibold'>{currencyFormat((productsPrice + shippingPrice) * (1 - discount))}</span>
+                  <span className="font-semibold text-puebla-dark">Descuento ({discount * 100}%)</span>
                 </div>
               </div>
-              <div className="bg-white w-fit p-3 rounded-md">
+              <div className="bg-white p-3 rounded-md w-full">
                 <h4 className="font-bold  text-xl mb-3">Datos de la cuenta:</h4>
-                <div className="grid grid-cols-2">
+
+                <div className="flex flex-row justify-between items-center w-full">
                   <span className="font-semibold">CVU:</span>
                   <div className="flex flex-row gap-2">
-                    <span>0000168300000003939550</span>
-                    <BiSolidCopy />
+                    <span>{cbuBanco}</span>
+                    <BiSolidCopy className="text-puebla-dark cursor-pointer" onClick={() => copyToClipboard(cbuBanco)} />
                   </div>
+                </div>
 
+                <div className="flex flex-row justify-between items-center w-full">
                   <span className="font-semibold">ALIAS:</span>
-                  <div className="flex flex-row gap-2">
-                    <span>nan.arevalo</span>
-                    <BiSolidCopy />
+                  <div className="flex flex-row gap-2 items-center">
+                    <span>{aliasBanco}</span>
+                    <BiSolidCopy className="text-puebla-dark cursor-pointer" onClick={() => copyToClipboard(aliasBanco)} />
                   </div>
+                </div>
 
+                <div className="flex flex-row justify-between items-center w-full">
                   <span className="font-semibold">NOMBRE:</span>
-                  <div className="flex flex-row gap-2">
-                    <span>HERNÁN ARÉVALO</span>
-                    <BiSolidCopy />
+                  <div className="flex flex-row gap-2 items-center uppercase">
+                    <span>{titularBanco}</span>
                   </div>
-
                 </div>
               </div>
-
+              <div className="bg-white p-3 rounded-md w-full border">
+                <h4 className="font-bold  text-xl mb-3">Enviar comprobante:</h4>
+                
+                <section className="container">
+                  <div {...getRootProps({ className: 'dropzone border-2 bg-gray-200 flex justify-center items-center p-3 text-center border-dashed border-gray-300 cursor-pointer'})}>
+                    <input {...getInputProps()} />
+                    <p>Agregá la imagen del comprobante acá!</p>
+                  </div>
+                  { acceptedFiles.length > 0 &&
+                  <aside className="mt-3 flex flex-col">
+                    <h4 className="font-semibold">Archivo:</h4>
+                    <span className="break-words">
+                      {acceptedFiles[0].name}
+                    </span>
+                    <div className="flex flex-row gap-2 items-center justify-center">
+                      <button className="btn-dark" onClick={() => {}}>Guardar comprobante</button>
+                    </div>
+                  </aside>
+                  }
+                </section>
+              </div>
             </div>
 
           </div>
