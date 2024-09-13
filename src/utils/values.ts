2a5a9db08996771480cd54cb4c144b0c5cfb6c76
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-09-13 14:01:49 -0300

    update values

--- src/utils/values.ts
+++ src/utils/values.ts
@@ -1,4 +1,7 @@
 
-export const discount = 0.90
-export const discounted = 0.10
+export const discount = 0.15
 export const deliveryValue = 3000
+
+export const aliasBanco = "PUEBLA.3D"
+export const cbuBanco = "0200912811000013531658"
+export const titularBanco = "Hernán Arévalo"
