68ec1cc8396de42a4703b49ad17277e4ad81f5aa
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-09-13 14:02:08 -0300

    update component styles

--- src/components/ui/divider/Divider.tsx
+++ src/components/ui/divider/Divider.tsx
@@ -1,10 +1,12 @@
 
 interface Params {
   color?: string
+  className?: string
+
 }
 
-export const Divider = ({ color = '#92C7D7'}: Params) => {
+export const Divider = ({ color = '#92C7D7', className}: Params) => {
   return (
-    <div className="w-full h-0.5 rounded mb-10 " style={{backgroundColor: color}}/>
+    <div className={`w-full h-0.5 rounded ${className}`} style={{backgroundColor: color}}/>
   )
 }
