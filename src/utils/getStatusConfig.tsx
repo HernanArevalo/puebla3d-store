e8dac27c2a2359260f44c82e78bd0c89824482a7
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-09-14 14:25:53 -0300

    add bg data

--- src/utils/getStatusConfig.tsx
+++ src/utils/getStatusConfig.tsx
@@ -10,43 +10,50 @@ export const getStatusConfig = ({status, iconSize=30}:Props) => {
     case 'PENDING':
       return {
         text: 'Pendiente de pago',
-        color: 'yellow-600',
+        color: 'text-yellow-600',
+        bg: 'bg-yellow-600',
         icon: <IoTimeOutline size={iconSize}/>
       };
     case 'PROCESSING':
       return {
         text: 'Procesando pago',
-        color: 'blue-600',
+        color: 'text-blue-600',
+        bg: 'bg-blue-600',
         icon: <IoCubeOutline size={iconSize} />,
       };
     case 'PRODUCTION':
       return {
         text: 'En producción',
-        color: 'purple-600',
+        color: 'text-purple-600',
+        bg: 'bg-purple-600',
         icon: <IoCubeOutline size={iconSize} />,
       };
     case 'SHIPPED':
       return {
         text: 'Enviado',
-        color: 'orange-600',
+        color: 'text-orange-600',
+        bg: 'bg-orange-600',
         icon: <IoPaperPlaneOutline size={iconSize} />,
       };
     case 'DELIVERED':
       return {
         text: 'Pedido entregado',
-        color: 'green-600',
+        color: 'text-green-600',
+        bg: 'bg-green-600',
         icon: <IoCheckmarkCircleOutline size={iconSize} />,
       };
     case 'CANCELLED':
       return {
         text: 'Pedido cancelado',
-        color: 'red-600',
+        color: 'text-red-600',
+        bg: 'bg-red-600',
         icon: <IoCloseCircleOutline size={iconSize} />,
       };
     default:
       return {
         text: 'Estado desconocido',
-        color: 'gray-600',
+        color: 'text-gray-600',
+        bg: 'bg-gray-600',
         icon: <IoTimeOutline size={iconSize} />,
       };
   }
